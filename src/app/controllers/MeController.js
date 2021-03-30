be8c7970c507e56fc1b0d6ff8bf1893bd4c89b7b
edward-pis-1670 <71159568+edward-pis-1670@users.noreply.github.com>
2021-03-30 16:51:04 +0700

    deleted count

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -4,13 +4,27 @@ const {multipleMongooseToObject} = require('../../util/mongoose')
 class MeController {
     //[GET] /me/stored/courses
 
+    
     storedCourses(req, res, next) {
-        Course.find({})
-            .then(courses => res.render('me/stored-courses', {
-                courses: multipleMongooseToObject(courses)
-            }))
-            .catch(next)
+        Promise.all([Course.find({}), Course.countDocumentsDeleted()])
+            .then(([courses, deletedCount]) =>
+                res.render('me/stored-courses', {
+                    deletedCount,
+                    courses: multipleMongooseToObject(courses)
+                }))
+                .catch(next)
     }
+
+
+    //     Course.countDocumentsDeleted()
+    //         .then(deletedCount => console.log(deletedCount))
+    //         .catch(next)
+    //     Course.find({})
+    //         .then(courses => res.render('me/stored-courses', {
+    //             courses: multipleMongooseToObject(courses)
+    //         }))
+    //         .catch(next)
+    // }
 
     //[GET] /me/trash/courses
     trashCourses(req, res, next) {
