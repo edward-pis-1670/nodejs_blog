7c8b128849f7ad2d57045e133125242a9856ece3
edward-pis-1670 <edward.pis.1670@gmail.com>
2021-03-30 22:30:18 +0700

    select all

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -7,6 +7,7 @@ const courseControllers = require('../app/controllers/CourseController');
 
 router.get('/create', courseControllers.create);
 router.post('/store', courseControllers.store);
+router.post('/handle-form-actions', courseControllers.handleFormActions);
 router.get('/:id/edit', courseControllers.edit);
 router.put('/:id', courseControllers.update);
 router.delete('/:id', courseControllers.destroy);
